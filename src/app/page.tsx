879d7669ee7f4213388c71c3d04784bf01775b78
larskros <i461382@FontysICT.nl>
2024-05-20 15:07:44 +0200

    Added the date input for 1st day of sickness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "./Components/Navbar/Navbar";
-import Footer from "./Components/Footer/Footer";
+import FirstDayOfSickness from "./Components/SickEmployee/FirstDayOfSickness";
 import { StepIndicator } from "./Components/Roadmap/StepIndicator";
+import Footer from "./Components/Footer/Footer";
 
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <div className={styles.ContentContainer}>
+        <div className={styles.ContentSectionLeft}>
+          {/* Component for page title and explanation */}
+          <FirstDayOfSickness />
+          {/* Component for questions about 'type verzuim' and 'standaard procedure doorlopen?' */}
+        </div>
+        <div className={styles.ContentSectionRight}>
+          {/* Component for the standard procedure text */}
+        </div>
+      </div>
       <div className="RoadmapContainer">
         <StepIndicator />
       </div>
-      {/* <div className={styles.empty}></div> */}
       <Footer />
 
     </>
