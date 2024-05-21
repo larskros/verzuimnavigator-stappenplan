b8ff8e93cfbda05ea08f6f3e52d96f45f1369dd0
nassimisohadouten <nassim1806@hotmail.com>
2024-05-21 10:34:20 +0200

    Added bulletpoints to correct component

--- src/app/components/standardprocedure/standardprocedure.tsx
+++ src/app/components/standardprocedure/standardprocedure.tsx
@@ -11,27 +11,6 @@ export default function Standardprocedure() {
           <h1>Zieke medewerker</h1>
           <p>Uw medewerker meldt zich ziek en verschijnt niet op het werk. Dit hoeft niet meteen iets ernstigs te betekenen, maar u moet als werkgever wel direct enkele stappen ondernemen om het goed te regelen. De volgende stappen gelden voor elk type ziekmelding en leggen de basis voor een gestructureerde opvolging en ondersteuning van de werknemer.</p>
         </div>
-
-        <div className={standardprocedure.procedure}>
-          <h1>Standaardprocedure voor ziekmelding</h1>
-          <div className={standardprocedure.step}>
-            <div className={standardprocedure.step_number}>1</div>
-            <div className={standardprocedure.step_content}>U vult de eerste dag (datum) van ziekmelding in. Vanaf hier gaat het stappenplan lopen.</div>
-          </div>
-          <div className={standardprocedure.step}>
-            <div className={standardprocedure.step_number}>2</div>
-            <div className={standardprocedure.step_content}>De werkgever verstrekt de werknemer het nodige verzuimformulier of meldingsdocument en de werknemer vult deze in. <a href="#" className={standardprocedure.download_link}><FontAwesomeIcon icon={faFileArrowDown} className={standardprocedure.FaIcons} /> Download Verzuimformulier</a></div>
-          </div>
-          <div className={standardprocedure.step}>
-            <div className={standardprocedure.step_number}>3</div>
-            <div className={standardprocedure.step_content}>De werkgever registreert de ziekmelding in het verzuimregistratiesysteem. Indien nodig, wordt de bedrijfsarts of arbodienst geïnformeerd.</div>
-          </div>
-          <div className={standardprocedure.step}>
-            <div className={standardprocedure.step_number}>4</div>
-            <div className={standardprocedure.step_content}>De werkgever neemt contact op met de zieke werknemer om het verdere verloop van het ziekteverzuim te bespreken en eventuele vervolgstappen af te stemmen.</div>
-          </div>
-        </div>
-        
         {}
       </div>
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Navbar from "./Components/Navbar/navbar";
-import standardprocedure from "./components/StandardProcedure/StandardProcedure.module.css";
-import Standardprocedure from "./Components/StandardProcedure/StandardProcedure";
+import Navbar from "./Components/Navbar/Navbar";
+import StandardProcedure from "./Components/StandardProcedure/StandardProcedure";
 import Bulletpoints from "./Components/BulletPoints/BulletPoints";
-import bulletpoints from "./components/BulletPoints/BulletPoints.module.css";
 import FirstDayOfSickness from "./Components/SickEmployee/FirstDayOfSickness";
 import { StepIndicator } from "./Components/Roadmap/StepIndicator";
-import Footer from "./Components/footer/footer";
+import Footer from "./Components/Footer/Footer";
 
 
 export default function Home() {
@@ -16,12 +14,12 @@ export default function Home() {
       <Navbar />
       <div className={styles.ContentContainer}>
         <div className={styles.ContentSectionLeft}>
-          {/* Component for page title and explanation */}
+          <StandardProcedure />
           <FirstDayOfSickness />
           {/* Component for questions about 'type verzuim' and 'standaard procedure doorlopen?' */}
         </div>
         <div className={styles.ContentSectionRight}>
-          {/* Component for the standard procedure text */}
+          <Bulletpoints />
         </div>
       </div>
       <div className="RoadmapContainer">
