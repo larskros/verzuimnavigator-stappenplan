025e9bd57fd960f4099780b06104e22a03b5788c
nassimisohadouten <nassim1806@hotmail.com>
2024-05-21 11:11:55 +0200

    Fixed css for standard procedure and bullet points

--- src/app/Components/StandardProcedure/StandardProcedure.tsx
+++ src/app/Components/StandardProcedure/StandardProcedure.tsx
@@ -6,8 +6,8 @@ import { faChevronDown, faFileArrowDown } from '@fortawesome/free-solid-svg-icon
 export default function Standardprocedure() {
   return (
     <>
-      <div className={standardprocedure.container}>
-        <div className={standardprocedure.titles}>
+      <div className={standardprocedure.Container}>
+        <div className={standardprocedure.Titles}>
           <h1>Zieke medewerker</h1>
           <p>Uw medewerker meldt zich ziek en verschijnt niet op het werk. Dit hoeft niet meteen iets ernstigs te betekenen, maar u moet als werkgever wel direct enkele stappen ondernemen om het goed te regelen. De volgende stappen gelden voor elk type ziekmelding en leggen de basis voor een gestructureerde opvolging en ondersteuning van de werknemer.</p>
         </div>
